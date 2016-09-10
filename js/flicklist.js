0e95260e319103ff812a3ca9ba4ad7fc9dd8bda7
Mariah Shields <mariah_shields@yahoo.com>
2016-09-10 05:38:28 +0000

    finish FlickList 2 studio

--- js/flicklist.js
+++ js/flicklist.js
@@ -28,11 +28,12 @@ function discoverMovies(callback) {
   $.ajax({
     url: api.root + "/discover/movie",
     data: {
-      api_key: api.token
+      api_key: "71549384b8353d116c81e30ba0329972"
     },
     success: function(response) {
       model.browseItems = response.results;
       callback();
+      console.log(response);
     }
   });
 }
@@ -51,6 +52,18 @@ function searchMovies(searchTerm, callback) {
   // TODO 9
   // implement this function as described in the comment above
   // you can use the body of discoverMovies as a jumping off point
+   $.ajax({
+    url: api.root + "/search/movie",
+    data: {
+      api_key: "71549384b8353d116c81e30ba0329972",
+      query: searchTerm
+    },
+    success: function(response) {
+      model.browseItems = response.results;
+      callback();
+
+    }
+  });
 
 
 }
@@ -72,6 +85,7 @@ function render() {
       .append(title)
       // TODO 3
       // give itemView a class attribute of "item-watchlist"
+      .attr("class", "item-watchlist");
 
     $("#section-watchlist ul").append(itemView);
   });
@@ -84,23 +98,26 @@ function render() {
       .click(function() {
         model.watchlistItems.push(movie);
         render();
-      });
+      })
       // TODO 2
       // the button should be disabled if this movie is already in
       // the user's watchlist
       // see jQuery .prop() and Array.indexOf()
+      .prop("disabled", model.watchlistItems.indexOf(movie) !== -1);
 
 
     // TODO 1
     // create a paragraph containing the movie object's .overview value
     // then, in the code block below,
     // append the paragraph in between the title and the button
+    var description = $("<p></p>").text(movie.overview);
 
 
     // append everything to itemView, along with an <hr/>
     var itemView = $("<li></li>")
       .append($("<hr/>"))
       .append(title)
+      .append(description)
       .append(button);
 
     // append the itemView to the list
